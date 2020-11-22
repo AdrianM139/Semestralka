4786bbefd6061827ca917db94c65614b864d17ca
AdrianM139 <adrian.masarovic@gmail.com>
2020-11-22 20:03:57 +0100

    editovanie príspevku a slideshow

--- App/Views/Blog/index.view.php
+++ App/Views/Blog/index.view.php
@@ -1,6 +1,11 @@
 <?php /** @var Array $data */
 ?>
-
+<div class="slideShow">
+    <img class="mySlides w3-animate-fading" src="img/lesy2.png" alt="">
+    <img class="mySlides w3-animate-fading" src="img/nar.parky2.png" alt="">
+    <img class="mySlides w3-animate-fading" src="img/z2.png" alt="">
+    <img class="mySlides w3-animate-fading" src="img/r0.png" alt="">
+</div>
 <div>
     <a href="?c=blog&a=pridaj">Pridaj dačo</a>
 
@@ -14,7 +19,6 @@
             <div class="col-md-4">
                 <h2><?=$article->getTitle() ?></h2>
                 <p><?=$article->getText() ?></p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details »</a>
                 <a class="btn btn-primary" href="?c=blog&a=uprav&id=<?=$article->getId() ?>" role="button">Upraviť</a>
                 <a class="btn btn-danger" href="?c=blog&zmaz=true&id=<?=$article->getId() ?>" role="button">Zmazať</a></p>
             </div>
@@ -23,4 +27,20 @@
         </div>
     </div>
 
+<script>
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}
+        x[myIndex-1].style.display = "block";
+        setTimeout(carousel, 8000);
+    }
+</script>
 
--- App/Views/Blog/uprav.view.php
+++ App/Views/Blog/uprav.view.php
@@ -6,8 +6,14 @@ $article = $data['articles'];
 <body>
 
 <form method="post">
-    <input type="text" name="title" value="<?= $article->getTitle() ?>">
-    <input type="text" name="text" value="<?= $article->getText() ?>">
+    <div class="form-group">
+        <label>Názov príspevku</label>
+        <input name="title" type="text" class="form-control" value="<?= $article->getTitle() ?>">
+    </div>
+    <div class="form-group">
+        <label>Text</label>
+        <textarea name="text" class="form-control" rows="3"><?= $article->getText() ?></textarea>
+    </div>
     <input type="submit" value="Odoslať">
 </form>
 
